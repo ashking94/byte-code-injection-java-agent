68979f2f9bc4f93a2cbf7907f5d011892e611a63
Ashish Singh <ashking94@gmail.com>
2023-06-23 10:11:06 +0530

    Add javaassist library and support for adding imports, before and after methods

--- src/main/java/com/example/MyAgent.java
+++ src/main/java/com/example/MyAgent.java
@@ -1,5 +1,9 @@
 package com.example;
 
+import javassist.ClassPool;
+import javassist.CtClass;
+import javassist.CtMethod;
+
 import java.lang.instrument.ClassFileTransformer;
 import java.lang.instrument.IllegalClassFormatException;
 import java.lang.instrument.Instrumentation;
@@ -7,7 +11,7 @@ import java.security.ProtectionDomain;
 
 public class MyAgent {
     public static void premain(String agentArgs, Instrumentation inst) {
-        inst.addTransformer(new MyTransformer());
+        inst.addTransformer(new LoggingClassFileTransformer());
     }
 
     static class MyTransformer implements ClassFileTransformer {
@@ -20,8 +24,55 @@ public class MyAgent {
                 byte[] classfileBuffer) throws IllegalClassFormatException {
 
             // Perform bytecode manipulation here using ASM or Byte Buddy
-            System.out.println("Starting manipulation");
+            System.out.println("Starting manipulation ==> " + className);
             return classfileBuffer; // Return the modified bytecode
         }
     }
+
+    static class LoggingClassFileTransformer implements ClassFileTransformer {
+
+        private static final String CLASS_TO_TRANSFORM = "com.example.App";
+        private static final String METHOD_TO_LOG = "sayHelloAgain";
+
+        @Override
+        public byte[] transform(
+                ClassLoader loader,
+                String className,
+                Class<?> classBeingRedefined,
+                ProtectionDomain protectionDomain,
+                byte[] classfileBuffer) {
+
+            byte[] transformedClass = classfileBuffer;
+
+            if (className.equals(CLASS_TO_TRANSFORM.replace(".", "/"))) {
+                try {
+                    ClassPool classPool = ClassPool.getDefault();
+                    CtClass ctClass = classPool.makeClass(new java.io.ByteArrayInputStream(classfileBuffer));
+
+                    CtMethod[] methods = ctClass.getDeclaredMethods();
+                    for (CtMethod method : methods) {
+                        if (method.getName().equals(METHOD_TO_LOG)) {
+                            // Add import statements to the class's classPool
+                            classPool.importPackage("java.time.LocalDateTime");
+
+                            // Add Java code snippet at the start of the method
+                            String codeSnippet = "System.out.println(\"nanoTime=\" + System.nanoTime() + \" currentTime=\" + LocalDateTime.now());";
+                            method.insertBefore(codeSnippet);
+                            method.insertAfter(codeSnippet);
+                        }
+                    }
+
+                    transformedClass = ctClass.toBytecode();
+                    ctClass.detach();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+            return transformedClass;
+        }
+    }
 }
+
+
+
